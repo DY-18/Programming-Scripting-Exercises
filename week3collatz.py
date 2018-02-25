624ceee1d38284038f48242db2b746b604678c21
Darren Young <36315711+DY-18@users.noreply.github.com>
2018-02-25 09:19:16 +0000

    Update week3collatz.py
    The following is the text from the Integrated Terminal when the week3.py python program for the collatz conjecture was executed.
    
    PS C:\Users\user\Desktop\WEEK 3 GMIT> python week3.py
    Please enter an integer:17
    52
    26
    13
    40
    20
    10
    5
    16
    8
    4
    2
    1

--- week3collatz.py
+++ week3collatz.py
@@ -1,1 +1,15 @@
+#Darren Young, 2018-12-11
+# Week 3 GMIT The Collatz Conjecture 
+# source code: modified from  https://stackoverflow.com/questions/13366830/collatz-conjecture-sequence
 
+n = int(input("Please enter an integer:"))
+
+while n > 1:
+        if n % 2 == 0 :
+            n = n // 2
+            print(n)
+        elif n % 2 == 1 :
+            n = 3 * n + 1
+            print(n)
+        if n == 1 :
+            break
