5d5a1d45a985ce00ea0b9af1d9478f140001e386
Darren Young <36315711+DY-18@users.noreply.github.com>
2018-03-24 02:03:34 +0000

    Create Exercise 6 Functions - factorial.py

---
+++ Exercise 6 Functions - factorial.py
@@ -0,0 +1,18 @@
+# Code originally from Sarah Scholz 
+# Function to find factorial of a given number
+
+def factorial(x):
+
+    f = 1
+
+    for i in range (1, x + 1):
+
+        f = i * f
+
+    return f
+
+print("The factorial of 5 is", factorial(5))
+
+print("The factorial of 7 is", factorial(7))
+
+print("The factorial of 10 is", factorial(10))
