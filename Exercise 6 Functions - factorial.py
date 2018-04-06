1d2601fe55b0e0dc408ab8080164e04fce35da2f
Darren Young <36315711+DY-18@users.noreply.github.com>
2018-04-06 21:44:51 +0100

    Update Exercise 6 Functions - factorial.py
    Added comments to explain the code

--- Exercise 6 Functions - factorial.py
+++ Exercise 6 Functions - factorial.py
@@ -1,13 +1,14 @@
-# https://www.youtube.com/watch?v=JwO_25S_eWE
+# https://www.youtube.com/watch?v=JwO_25S_eWE from Khan Academy Definining a factorial function
+# ttps://stackoverflow.com/questions/26499776/python-using-def-in-a-factorial-program
 
 # Modified from original code Sarah Scholz 04/03/2018
 # Function to find factorial of a given number
 
-def factorial(x):
+def factorial(x):  # Define a function and passing a parameter
 
-    n = 1
+    n = 1          # Declare a variable fact and set the initial value=1
 
-    for i in range (1, x + 1):
+    for i in range (1, x + 1):  # Using loop for iteration
 
         n = i * n
 
